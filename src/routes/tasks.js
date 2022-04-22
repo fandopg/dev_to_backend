b57555b9064bbc92a7a33abdfdc7f423d46f64ac
zzinue <zzinue@gmail.com>
2022-04-21 20:37:19 -0500

    crud

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -12,6 +12,11 @@ router.get("/", async(req, res, next) => {
         next(error)
     }
 })
+router.get("/:id", async(req, res, next) => {
+    const { id } = req.params
+    const payload = await task.getByID(id)
+    res.json({ success: true, payload })
+})
 router.post("/", async(req, res, next) => {
     try {
         const { title, description, completed } = req.body
@@ -20,6 +25,34 @@ router.post("/", async(req, res, next) => {
             success: true,
             message: "Task created successfully",
             payload: taskCreated
+        })
+    } catch (error) {
+        next(error)
+    }
+})
+router.put("/:id", async(req, res, next) => {
+
+    try {
+        const { id } = req.params
+        const { title, description, completed } = req.body
+        const taskUpdated = await task.update(id, { title, description, completed })
+        res.json({
+            success: true,
+            message: "Task updated successfully",
+            payload: taskUpdated
+        })
+    } catch (error) {
+        next(error)
+    }
+})
+router.delete("/:id", async(req, res, next) => {
+    try {
+        const { id } = req.params
+        const taskDeleted = await task.del(id)
+        res.json({
+            success: true,
+            message: "Task deleted successfully",
+            payload: taskDeleted
         })
     } catch (error) {
         next(error)
--- src/usecases/task/index.js
+++ src/usecases/task/index.js
@@ -3,6 +3,9 @@ const Task = require("../../models/tasks").model
 const getAll = async() => {
     return await Task.find({}).exec()
 }
+const getByID = async(id) => {
+    return await Task.findById(id).exec()
+}
 const create = async(taskData) => {
     const { title, description, completed } = taskData
     const newTask = new Task({
@@ -12,4 +15,16 @@ const create = async(taskData) => {
     })
     return await newTask.save()
 }
-module.exports = { getAll, create }
+const update = async(id, taskData) => {
+    const { title, description, completed } = taskData
+    const updatedTask = await Task.findByIdAndUpdate(id, {
+        title,
+        description,
+        completed
+    }, { new: true }).exec()
+    return updatedTask
+}
+const del = async(id) => {
+    return await Task.findByIdAndRemove(id).exec()
+}
+module.exports = { getAll, getByID, create, update, del }
